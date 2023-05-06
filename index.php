eec05988f79200f186a633a33012f7f01c65c050
Zanetti Jordan <jzanetti1@outlook.fr>
2023-05-06 12:16:06 +0200

    Add graphics charts, fonts, images

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <?php include 'head.phtml'; ?>
+    </head>
+
+    <body>
+
+    <header>
+            <?php include 'header.phtml'; ?>
+    </header>
+
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <footer>
+        <?php include 'footer.phtml'; ?>
+    </footer>
+
+    <script type="text/javascript" src="scripts/main.js"></script>
+    </body>
